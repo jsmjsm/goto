7b037c4830b54240f6f9c7f41ab256c290a832b8
levy pan <levy-pan@outlook.com>
2021-03-08 19:44:14 +0800

    goto_v4 init

---
+++ goto_v4/store.go
@@ -0,0 +1,9 @@
+/*
+ * @Description  : manage to store the data
+ * @Author       : jsmjsm
+ * @Github       : https://github.com/jsmjsm
+ * @Date         : 2021-03-08 19:30:23
+ * @LastEditors  : jsmjsm
+ * @LastEditTime : 2021-03-08 19:43:58
+ * @FilePath     : /goto/goto_v4/store.go
+ */
